0ffaacbf0c0ab506cd1e6828596f808392c8efa8
Dmitry Stelmakhov <jaiz@yandex.ru>
2022-06-26 01:49:22 +0300

    new

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('У меня получилось!')
+    return HttpResponse('Я запустил сервер!')
 
 
 def second_page(request):
